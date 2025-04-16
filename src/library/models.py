c249d07200cae9e792c5eadfe34147d3225304d2
Rafael-129 <rafael.garcia.c@tecsup.edu.pe>
2025-04-16 14:33:14 -0500

    Class Category

--- src/library/models.py
+++ src/library/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class Category(models.Model):
+    """Book category/genre"""
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    
+    class Meta:
+        verbose_name_plural = "categories"
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
