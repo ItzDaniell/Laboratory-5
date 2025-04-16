c307784687c550836df1024975f43dab0ce1dedc
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-04-16 14:14:52 -0500

    TestAuthor

--- src/library/tests.py
+++ src/library/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Author, Book, Category, Publisher, Publication
 
-# Create your tests here.
+class AuthorModelTest(TestCase):
+    """Test cases for the Author model"""
+    def setUp(self):
+        """Create test author"""
+        self.author = Author.objects.create(
+            name="J.K. Rowling",
+            birth_date="1965-07-31"
+        )
